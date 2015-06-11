4928e9bc756d3e672497d6eed6a63afc14b2ee13
Lone Leamer <leamerluck@gmail.com>
2015-06-11 12:13:27 -0400

    Initial commit

---
+++ card.rb
@@ -0,0 +1,75 @@
+
+require "pry"
+
+class Card
+	attr_accessor :rank, :suit
+
+	def initialize(rank, suit)
+		self.rank = rank
+		self.suit = suit
+	end
+
+	def output_card
+		puts "#{self.rank} of #{self.suit}"
+	end
+
+end
+
+
+class Deck
+	def initialize
+		@cards = []
+		ranks = [2,3,4,5,6,7,8,9,10, "Jack", "Queen", "King", "Ace"]
+		suits = ["spades", "diamonds", "hearts", "clubs"]
+		
+		ranks.each do |rank|
+			suits.each do |suit|
+				@cards << Card.new(rank,suit)
+			end
+		end
+
+		
+	end
+
+	def output
+		@cards.each do |card|
+			card.output_card
+		end
+	end
+
+	def shuffle
+		@cards.shuffle!
+	end
+
+	def deal
+		@cards.shift
+	end
+
+	def count
+		@cards.count
+	end
+end
+
+deck = Deck.new
+deck.shuffle
+
+puts "Cards are shuffled. Type \"deal\" if you'd like to have a card dealt."
+deal = "deal"
+
+while deal != "quit" && deck.count > 0
+	print "> "
+	deal = $stdin.gets.chomp.downcase
+
+	if deal == "deal"
+		user_card = deck.deal
+		user_card.output_card
+		if deck.count > 0
+			puts "#{deck.count} cards remain. Deal again or quit?"
+		else 
+			puts "Game over."
+		end
+	elsif deal != "quit"
+		puts "Type \"deal\" to deal a card or \"quit\" to quit."
+	end
+end
+
---
+++ image-blur.rb
@@ -0,0 +1,23 @@
+require "pry"
+
+class Image
+	attr_accessor :image 
+
+	def initialize(image)
+		@image = image
+	end
+
+	def output_image
+		@image.each do |subarry|
+			puts subarry.join
+		end
+	end
+end
+
+image = Image.new([
+			[0,0,0,0],
+			[0,1,0,0],
+			[0,0,0,1],
+			[0,0,0,0]
+		])
+image.output_image
