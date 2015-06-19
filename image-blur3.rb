242424570b50dafce77df37982836c11570a277e
Lone Leamer <leamerluck@gmail.com>
2015-06-19 08:42:22 -0400

    refactored Manhattan distance [x,y]

--- image-blur3.rb
+++ image-blur3.rb
@@ -11,46 +11,48 @@ class Image
 		@ones_indexes = Array.new
 		@image.each_with_index do |row, row_index|
 			row.each_with_index do |num, num_index|
-					@ones_indexes.push(row_index,num_index) if num == 1 
+					@ones_indexes << [row_index,num_index] if num == 1 
 			end
 		end
 	end
 
 	def change_adjacents
 		@ones_indexes.each do |y,x|
-				y = @ones_indexes.shift
-				x = @ones_indexes.shift
-				puts image[5][3]
-				image[y-1][x] = 1 if image[y-1][x] != nil #left
-				image[y+1][x] = 1 if image[y+1][x] != nil #right
-				image[y][x-1] = 1 if image[y][x-1] != nil #down
-				image[y][x+1] = 1 if image[y][x+1] != nil #up
+					image[y-1][x] = 1 unless y == 0 || image[y-1][x] == nil #up
+					image[y+1][x] = 1 unless y == @image.length - 1 || image[y+1][x] == nil #down
+					image[y][x-1] = 1 unless x == 0 || image[y][x-1] == nil #right
+					image[y][x+1] = 1 unless x == @image[y].length - 1 || image[y][x+1] == nil #left
 		end
 	end
 
 	def blur(distance)
-		distance.times do
-			print index_of_ones
+		(distance).times do
+			index_of_ones
 			change_adjacents
 		end
 	end
 
 	def output_image
 		@image.each do |subarry|
-			puts subarry.join
+			puts subarry.join(' ')
 		end
 	end
 		
 end
 
 image = Image.new([
-			[0,0,0,0,0],
-			[0,0,0,0,0],
-			[0,0,0,0,0],
-			[0,0,1,0,0],
-			[0,0,0,0,0],
-			[0,0,0,0,0]
-
+			[0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0],
+			[0,0,0,1,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,1]			
 		])
 
 image.blur(3)
